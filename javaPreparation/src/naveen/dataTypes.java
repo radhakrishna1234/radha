5995f23e2fdf9577da41390f107cd57ccbc239af
radhakrishna <rgudimet@opentext.com>
2022-09-18 15:38:22 +0530

    methods added

---
+++ javaPreparation/src/naveen/dataTypes.java
@@ -0,0 +1,40 @@
+package naveen;
+
+public class dataTypes {
+	
+	boolean age;
+	
+	static char name = 'v';
+	static byte b = 1;
+	static short s = 2;
+	static int i = 3;
+	static long l = 4;
+	static float f = 10.22f;
+	static double d =45.54;
+	
+
+
+	public static void main(String[] args) {
+		System.out.println(name);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
+		l = 10;
+		f = 11.22f;
+		
+		System.out.println(name);
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		
+
+	}
+
+}
