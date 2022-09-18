13c735849c4b8657fef627154b190b39aefd28d8
radhakrishna <rgudimet@opentext.com>
2022-09-18 15:29:40 +0530

    methods added

---
+++ javaPreparation/src/naveen/Methods.java
@@ -0,0 +1,35 @@
+package naveen;
+
+public class Methods {
+	
+	
+	 static int a = 10;
+	 static	int b = 10;
+	
+	 
+	static public void add() {
+		 System.out.println(a+b);
+	 }
+	
+	static public void sub() {
+		 System.out.println(a-b);
+	 }
+	 
+	static public void mul() {
+		 System.out.println(a*b);
+	 }
+	 
+	static public void div() {
+		 System.out.println(a/b);
+	 }
+
+
+	public static void main(String[] args) {
+		add();
+		sub();
+		mul();
+		div();
+
+	}
+
+}
