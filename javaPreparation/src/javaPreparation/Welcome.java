27a3ac2ff5e25d4b17a6eeefe78f3ac6c0a12e6a
radhakrishna <rgudimet@opentext.com>
2022-09-18 14:11:50 +0530

    test1

---
+++ javaPreparation/src/javaPreparation/Welcome.java
@@ -0,0 +1,11 @@
+package javaPreparation;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Welcome ");
+
+	}
+
+}
---
+++ javaPreparation/src/naveen/WelComeRadha.java
@@ -0,0 +1,10 @@
+package naveen;
+
+public class WelComeRadha {
+
+	public static void main(String[] args) {
+		System.out.println(" I am krishna ");
+
+	}
+
+}
